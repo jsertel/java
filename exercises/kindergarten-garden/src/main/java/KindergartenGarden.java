d98560187ed91f6b7dd4a311875dc0367caed3b4
Scott Ertel <jsecpa@gmail.com>
2018-04-30 16:53:46 -0500

    removed unused constructor from starter implementation and reference solution

--- exercises/kindergarten-garden/src/main/java/KindergartenGarden.java
+++ exercises/kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -1,17 +1,51 @@
+//import java.util.List;
+//
+//class KindergartenGarden {
+//
+//    KindergartenGarden(String garden) {
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+//    }
+//
+//    List<Plant> getPlantsOfStudent(String student) {
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+//    }
+//
+//}
+import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
+
 
 class KindergartenGarden {
+    private String garden;
+    private static final int PLANTS_PER_STUDENT_PER_ROW = 2;
+    private final int newRowLocation;
 
-    KindergartenGarden(String garden, String[] students) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+    private static final String[] STUDENTS = {
+            "Alice", "Bob", "Charlie", "David",
+            "Eve", "Fred", "Ginny", "Harriet",
+            "Ileana", "Joseph", "Kincaid", "Larry"
+    };
+
+
 
     KindergartenGarden(String garden) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.garden = garden;
     }
 
     List<Plant> getPlantsOfStudent(String student) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        List<Plant> plants = new ArrayList<Plant>();
+        int studentPlantsIndex = Arrays.binarySearch(STUDENTS, student) * PLANTS_PER_STUDENT_PER_ROW;
 
+        for (int i = studentPlantsIndex; i < studentPlantsIndex + PLANTS_PER_STUDENT_PER_ROW; i++) {
+            plants.add(Plant.getPlant(garden.charAt(i)));
+        }
+
+        for (int i = newRowLocation + studentPlantsIndex;
+             i < newRowLocation + studentPlantsIndex + PLANTS_PER_STUDENT_PER_ROW; i++) {
+            plants.add(Plant.getPlant(garden.charAt(i)));
+        }
+
+        return plants;
+    }
 }
